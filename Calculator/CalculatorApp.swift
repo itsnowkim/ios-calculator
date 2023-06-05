eb65d3e14168e5e2e42e9b633e06b893a3fd5e38
itsnowkim <peterhyunjae@naver.com>
2023-06-05 20:50:31 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Now Kim on 2023/06/05.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
